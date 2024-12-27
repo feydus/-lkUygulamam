f1e3a0da779ba56dda95934031293d8d0295797c
Feyduş Kıymet Kocahan <feyduskocahann@gmail.com>
2024-12-27 03:13:09 +0300

    Initial Commit

---
+++ ılkUygulamam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ılkUygulamam
+//
+//  Created by Feyduş Kıymet Kocahan on 27.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ılkUygulamamTests/_lkUygulamamTests.swift
@@ -0,0 +1,17 @@
+//
+//  _lkUygulamamTests.swift
+//  ılkUygulamamTests
+//
+//  Created by Feyduş Kıymet Kocahan on 27.12.2024.
+//
+
+import Testing
+@testable import _lkUygulamam
+
+struct _lkUygulamamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
